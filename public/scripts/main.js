228248a91068fcab6341441771f7126b70a5174f
André de Alencar <andre.alencar.p@gmail.com>
2016-09-01 10:45:16 -0300

    canvas size part 3

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -78,5 +78,5 @@
 	ctx.canvas.width  = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
 	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(0,0,canvas.width,canvas.height);
+	ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height);
 })();
